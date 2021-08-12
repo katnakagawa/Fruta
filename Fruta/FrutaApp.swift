6e9a4a9eac496734b0650af699a378e0227d95f5
katsuya nakagawa <katnakagawa.m@gmail.com>
2021-08-12 13:25:46 +0900

    Initial Commit

---
+++ Fruta/FrutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutaApp.swift
+//  Fruta
+//
+//  Created by Katsuya Nakagawa on 2021/08/12.
+//
+
+import SwiftUI
+
+@main
+struct FrutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
